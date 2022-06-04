8c4b121f2007ad6bbf44eae3aae50a3aba681185
marcelinleen <94484224+marcelinleen@users.noreply.github.com>
2022-06-04 22:52:47 +0300

    changes

---
+++ selectors.py
@@ -0,0 +1,13 @@
+import re
+from selenium import webdriver
+import time
+import math
+
+try:
+    x = 10
+
+finally:
+    time.sleep(10) # give 10 sec before browser will close
+    browser.quit() # exit browser
+
+#empty
